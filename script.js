e0441977f71af7edb192eb676f1b59a967d62af0
Bullet <93942505+Bullet1289@users.noreply.github.com>
2023-03-17 22:52:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+
+// Получаем элементы HTML
+const addCommentForm = document.querySelector('.add-comment-form');
+const commentsList = document.querySelector('.comments-list');
+
+// Обработчик отправки формы
+addCommentForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Получаем значения полей формы
+    const name = addCommentForm.elements.name.value;
+    const text = addCommentForm.elements.text.value;
+    const date = addCommentForm.elements.date.value;
+
+    // Валидация формы
+    if (!name) {
+        showError(addCommentForm.elements.name, 'Введите имя');
+        return;
+    }
+
+    if (!text) {
+        showError(addCommentForm.elements.text, 'Введите текст');
+        return;
+    }
+
+    // Создаем элемент комментария
+    const comment = document.createElement('li');
+    comment.classList.add('comment');
+
+    // Создаем элементы для отображения информации о комментарии
+    const author = document.createElement('span');
+    author.classList.add('comment-author');
+    author.textContent = name;
+
+    const commentDate = document.createElement('span');
+    commentDate.classList.add('comment-date');
+    commentDate.textContent = formatDate(date);
+
+    const commentText = document.createElement('p');
+    commentText.classList.add('comment-text');
+    commentText.textContent = text;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-comment-button');
+    deleteButton.innerHTML = '&#128465;';
+
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('like-comment-button');
+    likeButton.innerHTML = '&#10084;';
+
+    // Добавляем элементы в комментарий
+    comment.appendChild(author);
+    comment.appendChild(commentDate);
+    comment.appendChild(commentText);
+    comment.appendChild(deleteButton);
+    comment.appendChild(likeButton);
+
+    // Добавляем комментарий в список
+    commentsList.appendChild(comment);
+
+    // Очищаем форму
+    addCommentForm.reset();
+});
+
+// Обработчик изменения значения поля
+addCommentForm.addEventListener('input', (event) => {
+    const input = event.target;
+
+    // Скрываем ошибку, если она была отображена
+    hideError(input);
+});
+
+// Обработчик клика по кнопке удаления комментария
+commentsList.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('delete-comment-button')) {
+        const comment = target.closest('.comment');
+        comment.remove();
+    }
+});
+
+// Обработчик клика по кнопке лайка комментария
+commentsList.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('like-comment-button')) {
+        target.classList.toggle('liked');
+    }
+});
+
+// Функция отображения ошибки
+function showError(input, message) {
+    const error = document.createElement('span');
+    error.classList.add('error-message');
+    error.textContent = message;
+    input.insertAdjacentElement('afterend', error);
+}
+
+// Функция скрытия ошибки
+function hideError(input) {
+    const error = input.nextElementSibling;
+
+    if (error && error.classList.contains('error-message')) {
+        error.remove();
+    }
+}
+
+// Функция форматирования даты комментария
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const today = new Date();
+
+    if (date.toDateString() === today.toDateString()) {
+        return 'Сегодня, ' + formatTime(date);
+    } else if (date.toDateString() === new Date(today.setDate(today.getDate() - 1)).toDateString()) {
+        return 'Вчера, ' + formatTime(date);
+    } else {
+        return date.toLocaleDateString() + ', ' + formatTime(date);
+    }
+}
+
+// Функция форматирования времени комментария
+function formatTime(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    return hours + ':' + minutes;
+}
